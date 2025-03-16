3a549ce69105abe91ca79fbe6eb5aa2f50b060e3
EmanZahid123 <cosmicvoyager2003@gmail.com>
2025-03-16 12:03:53 +0500

    function to generate short urls

--- app.py
+++ app.py
@@ -10,3 +10,7 @@ app.config.from_object('config.Config')
 # Initialize MongoDB connection
 mongo = PyMongo(app)
 db = mongo.db
+
+#Generate short url
+def generate_short_code():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=6))
